f08a966d6650bd333a3a10055f7c9a31d62221a6
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-09-07 12:01:32 +0300

    finally started the skeleton for the main file through which producer will start

---
+++ producer_service/main.py
@@ -0,0 +1,13 @@
+import os
+from config import PODCASTS_DIR
+from file_reader import extract_metadata
+from json_builder import build_json
+from kafka_producer import send_to_kafka
+
+def main():
+    """
+    the entry point for producer Service
+    """
+
+if __name__ == "__main__":
+    main()
