ec4d0a13fc6ef1ea4d874463e4b8bdc44f6f63b5
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-09-07 15:42:23 +0300

    i started again with a config file for the settings

---
+++ consumer_service/config.py
@@ -0,0 +1,14 @@
+import os
+
+# the kafka configuration
+KAFKA_BROKER = os.getenv("KAFKA_BROKER", "kafka:9092")
+KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "podcast-files")
+
+# elasticsearch configuration
+ES_HOST = os.getenv("ES_HOST", "http://elasticsearch:9200")
+ES_INDEX = os.getenv("ES_INDEX", "podcasts_metadata")
+
+# mongoDB configuration
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongo:27017/")
+MONGO_DB = os.getenv("MONGO_DB", "podcasts_db")
+MONGO_COLLECTION = os.getenv("MONGO_COLLECTION", "podcasts_files")
