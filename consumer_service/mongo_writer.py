467b409917dbac3cd10c6aa332d5dd45f8025454
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-09-07 16:48:36 +0300

    also implemented the func responsible for saving the data files (podcasts) in Mongo. I used mongo internal library, gridfs, which knows how toconvert by dividing the informetion into bits and saving according to the data and metadata, so we can also request the information back

--- consumer_service/mongo_writer.py
+++ consumer_service/mongo_writer.py
@@ -7,7 +7,11 @@ def save_file_to_mongo(unique_id: str, file_path: str):
     """
     here save the actual .wav file into mongoDB using GridFS
     """
-    
-    client = ...
-    db = ...
-    fs = ...
+    client = pymongo.MongoClient(MONGO_URI)
+    db = client[MONGO_DB]
+    fs = gridfs.GridFS(db, collection=MONGO_COLLECTION)
+
+    path = pathlib.Path(file_path)
+    with open(path, "rb") as f:
+        file_id = fs.put(f, filename=path.name, unique_id=unique_id)
+        print(f"[MongoDB] Stored file {path.name} with unique_id={unique_id}, file_id={file_id}")
