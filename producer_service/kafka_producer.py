bf24570c9975c779ddbd37d507ffbc33a2b26762
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-09-07 12:41:36 +0300

    I implemented the Kafka producer, simply by converting the json to UTF 8 format, and at this point it is actually produced to the broker, and also update the requiremments

--- producer_service/kafka_producer.py
+++ producer_service/kafka_producer.py
@@ -1,9 +1,19 @@
 from kafka import KafkaProducer
 from config import KAFKA_BROKER, KAFKA_TOPIC
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 def send_to_kafka(json_message: str):
     """
     now i send json message to Kafka topic
     """
-    ...
-
+    producer = KafkaProducer(
+        # here is the list of kafka brokers
+        bootstrap_servers=[KAFKA_BROKER],
+        # serialize json message to utf-8
+        value_serializer=lambda v: v.encode("utf-8")
+    )
+    producer.send(KAFKA_TOPIC, json_message)
+    producer.flush()
+    logging.info(f"Sent message to Kafka topic {KAFKA_TOPIC}")
