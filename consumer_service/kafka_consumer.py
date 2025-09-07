b1be8ed01cac8779f56081f6b0db98b00bbe40d8
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-09-07 16:54:36 +0300

    I implemented a Kafka consumer, and now models can process them after it has been processed into sorted json files that are returned to Kafka

--- consumer_service/kafka_consumer.py
+++ consumer_service/kafka_consumer.py
@@ -7,5 +7,12 @@ def consume_messages():
     finally consume messages from kafka topic and yield json objects
     so that other modules can process them
     """
-    ...
-
+    consumer = KafkaConsumer(
+        KAFKA_TOPIC,
+        bootstrap_servers=[KAFKA_BROKER],
+        value_deserializer=lambda v: json.loads(v.decode("utf-8")),
+        auto_offset_reset="earliest",
+        group_id="podcast-consumer-group"
+    )
+    for message in consumer:
+        yield message.value
