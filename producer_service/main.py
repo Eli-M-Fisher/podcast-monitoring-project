2beca5cb82abd5fc227b4827d8b8113a873e7de9
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-09-07 13:00:29 +0300

    I finally implemented the main executable file thet will actually run everything we built. It accesses the path location where there are files and looks for a suitable extnsion, and runs the reader. the conversion is done to json and the json is sent to Kafka

--- producer_service/main.py
+++ producer_service/main.py
@@ -8,6 +8,12 @@ def main():
     """
     the entry point for producer Service
     """
+    for file_name in os.listdir(PODCASTS_DIR):
+        if file_name.endswith(".wav"):
+            file_path = os.path.join(PODCASTS_DIR, file_name)
+            metadata = extract_metadata(file_path)
+            json_message = build_json(metadata)
+            send_to_kafka(json_message)
 
 if __name__ == "__main__":
     main()
