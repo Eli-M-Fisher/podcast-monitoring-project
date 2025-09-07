939dbaea149f17596f422ef9559eeb3e57bc8a21
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-09-07 11:41:28 +0300

    I added a configuration file for general produser settings in 'External'

---
+++ producer_service/config.py
@@ -0,0 +1,8 @@
+import os
+
+# here is a path to local podcasts directory
+PODCASTS_DIR = os.getenv("PODCASTS_DIR", "/Users/elifisher/Downloads/podcasts")
+
+# and kafka configuration
+KAFKA_BROKER = os.getenv("KAFKA_BROKER", "kafka:9092")
+KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "podcast-files")
