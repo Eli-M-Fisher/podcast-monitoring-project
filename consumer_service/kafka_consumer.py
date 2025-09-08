f2b430fd3e0937c91eddacaba9b8f16563421c15
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-09-08 14:59:27 +0300

    add logs to kafka consumer

--- consumer_service/kafka_consumer.py
+++ consumer_service/kafka_consumer.py
@@ -1,6 +1,9 @@
 from kafka import KafkaConsumer
 import json
 from config import KAFKA_BROKER, KAFKA_TOPIC
+from common.logger import Logger
+
+logger = Logger.get_logger()
 
 def consume_messages():
     """
@@ -8,16 +11,25 @@ def consume_messages():
     so that other modules can process them
     """
     # here is the kafka consumer that reads from the topic
-    consumer = KafkaConsumer(
-        KAFKA_TOPIC,
-        # here is the list of kafka brokers (just one in my case now) (bootstrap means starting point (l.f))
-        bootstrap_servers=[KAFKA_BROKER],
-        # deserialize json message from utf-8 (string) to dict
-        value_deserializer=lambda v: json.loads(v.decode("utf-8")),
-        # and start reading at the earliest message (if no offset is committed yet)
-        auto_offset_reset="earliest",
-        # and use this consumer group id (so that multiple consumers can share the load)
-        group_id="podcast-consumer-group"
-    )
-    for message in consumer:
-        yield message.value
+    try:
+        consumer = KafkaConsumer(
+            KAFKA_TOPIC,
+            # here is the list of kafka brokers (just one in my case now) (bootstrap means starting point (l.f))
+            bootstrap_servers=[KAFKA_BROKER],
+            # deserialize json message from utf-8 (string) to dict
+            value_deserializer=lambda v: json.loads(v.decode("utf-8")),
+            # and start reading at the earliest message (if no offset is committed yet)
+            auto_offset_reset="earliest",
+            # and use this consumer group id (so that multiple consumers can share the load)
+            group_id="podcast-consumer-group"
+        )
+        logger.info(f"Connected to Kafka broker={KAFKA_BROKER}, topic={KAFKA_TOPIC}")
+
+        # now continuously listen for messages (and yield them as they come)
+        for message in consumer:
+            logger.info(f"Consumed message from Kafka topic={KAFKA_TOPIC}, partition={message.partition}, offset={message.offset}")
+            yield message.value
+
+    except Exception as e:
+        logger.error(f"Kafka consumer failed to connect or read messages: {e}")
+        raise
