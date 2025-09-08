ca18528803b3b713b471c1886dfb3c5d81e717fe
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-09-08 15:02:50 +0300

    add logs to mongo writer

--- consumer_service/mongo_writer.py
+++ consumer_service/mongo_writer.py
@@ -2,20 +2,30 @@ import pymongo
 import pathlib
 import gridfs # i disided to use gridfs to handle large files in mongodb because podcast .wav files can be large 
 from config import MONGO_URI, MONGO_DB, MONGO_COLLECTION
+from common.logger import Logger
+
+logger = Logger.get_logger()
 
 def save_file_to_mongo(unique_id: str, file_path: str):
     """
     here save the actual .wav file into mongoDB using GridFS
     """
-    # connect to mongoDb
-    client = pymongo.MongoClient(MONGO_URI)
-    db = client[MONGO_DB]
-    # use gridfs to handle large files
-    fs = gridfs.GridFS(db, collection=MONGO_COLLECTION)
+    try:
+        # connect to mongoDb
+        client = pymongo.MongoClient(MONGO_URI)
+        db = client[MONGO_DB]
+        # use gridfs to handle large files
+        fs = gridfs.GridFS(db, collection=MONGO_COLLECTION)
+        logger.info(f"Connected to MongoDB at {MONGO_URI}, db={MONGO_DB}, collection={MONGO_COLLECTION}")
 
-    # read the file and save to mongodB
-    path = pathlib.Path(file_path)
-    with open(path, "rb") as f: # ("rb") to read binary files like .wav (l.f)
-        # store the file with unique_id as metadata
-        file_id = fs.put(f, filename=path.name, unique_id=unique_id)
-        print(f"[MongoDB] Stored file {path.name} with unique_id={unique_id}, file_id={file_id}")
+        # read the file and save to mongodB
+        path = pathlib.Path(file_path)
+        with open(path, "rb") as f: # ("rb") to read binary files like .wav (l.f)
+            # store the file with unique_id as metadata
+            file_id = fs.put(f, filename=path.name, unique_id=unique_id)
+            logger.info(f"Stored file in MongoDB: filename={path.name}, unique_id={unique_id}, file_id={file_id}")
+            return file_id
+    
+    except Exception as e:
+        logger.error(f"Failed to save file {file_path} to mongodb (unique_id={unique_id}): {e}")
+        raise
