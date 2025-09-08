d963130bba7a426dd4d58e2b68519307c1e31f8d
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-09-08 15:10:04 +0300

    add logs to json builder

--- producer_service/json_builder.py
+++ producer_service/json_builder.py
@@ -1,8 +1,15 @@
 import json
+from common.logger import Logger
 
-def build_json(metadata: dict):
+logger = Logger.get_logger()
+
+def build_json(metadata: dict) -> str:
     """
     and convert metadata dictionary into json string
     """
     # here i simply convert the metadata dictionary to a json string
-    return json.dumps(metadata)
+    try:
+        return json.dumps(metadata)
+    except Exception as e:
+        logger.error(f"Failed to build json from metadata; {e}")
+        raise
