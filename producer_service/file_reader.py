ac443452e0ca85ec2f7100dc8288df6efa249fff
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-09-08 15:07:56 +0300

    add logs to file reader

--- producer_service/file_reader.py
+++ producer_service/file_reader.py
@@ -1,15 +1,19 @@
 import pathlib
 from constants import ERROR_FILE_NOT_FOUND
+from common.logger import Logger
+
+logger = Logger.get_logger()
 
 def extract_metadata(file_path: str) -> dict:
     """
     here i extract metadata from a file path
     """
-
     # i check if the file exists
     path = pathlib.Path(file_path)
+    logger.info(f"Tried to extract metadata for file: {file_path}")
 
     if not path.exists():
+        logger.error(f"{ERROR_FILE_NOT_FOUND}: {file_path}")
         raise FileNotFoundError(f"{ERROR_FILE_NOT_FOUND}: {file_path}")
     
     # extract some basic metadata from the file path
@@ -19,4 +23,6 @@ def extract_metadata(file_path: str) -> dict:
         "created_at": path.stat().st_ctime,
         "path": str(path.resolve())
     }
+    
+    logger.info(f"Extracted metadata for file={path.name}, size={metadata['file_size']} bytes")
     return metadata
