292e4f7bdea495fcd415e5294f70e6a03592449a
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-09-10 12:28:13 +0300

    I changed the logic from the information index in the consumer to update, so that even information that is overwritten will always only upload the final one, so that in the other services the latest version will be passed

--- consumer_service/elastic_writer.py
+++ consumer_service/elastic_writer.py
@@ -21,9 +21,15 @@ def write_metadata_to_es(unique_id: str, metadata: dict):
             "ingested_at": datetime.now(timezone.utc).isoformat()
         }
 
-        # index the document into elasticsearch
-        es.index(index=ES_INDEX, id=unique_id, document=doc)
-        logger.info(f"Indexed metadata in Elasticsearch (ID={unique_id})")
+        # upsert the document (insert or update if exists) use update with doc_as_upsert to avoid overwriting existing fields
+        es.update(
+            index=ES_INDEX,
+            id=unique_id,
+            body={"doc": doc},
+            doc_as_upsert=True
+        )
+
+        logger.info(f"Upserted metadata in Elasticsearch (ID={unique_id})")
 
     except Exception as e:
         logger.error(f"Failed to index metadata in Elasticsearch (ID={unique_id}): {e}")
