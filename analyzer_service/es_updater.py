ac686c372071b3d365d39a8b8acbe3314a0b1459
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-09-10 10:13:14 +0300

    now I have also implemented updating es in the document with bds_percent, is_bds, bds_threat level

--- analyzer_service/es_updater.py
+++ analyzer_service/es_updater.py
@@ -5,4 +5,21 @@ from common.logger import Logger
 logger = Logger.get_logger()
 
 def update_analysis(unique_id: str, analysis: dict):
-    ...
+    """
+    also update the Elasticsearch document with bds analysis results..
+    """
+    # connect to elasticsearch
+    es = Elasticsearch(ES_HOST)
+    try:
+        # update the document with new fields for bds analysis results (bds_percent, is_bds, bds_threat_level)
+        es.update(
+            index=ES_INDEX,
+            id=unique_id,
+            doc={"bds_percent": analysis["bds_percent"],
+                 "is_bds": analysis["is_bds"],
+                 "bds_threat_level": analysis["bds_threat_level"]}
+        )
+        logger.info(f"updated BDS analysis for ID={unique_id}")
+    except Exception as e:
+        logger.error(f"Failed to update BDs analysis for id={unique_id}: {e}")
+        raise
